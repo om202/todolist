43ec76ef8fb179735b8678abe10e8be42de3ee96
om202 <opsworld.g@gmail.com>
2023-04-22 16:52:35 -0600

    ready server

--- src/server/main.js
+++ src/server/main.js
@@ -12,17 +12,40 @@ const dbConfig = {
   DB: "todo_db",
 };
 
-async function initial() {
-  try {
-    let count = await Role.find({}).count();
-    if (count === 0) {
-      new Role({ name: "user" }).save();
-      new Role({ name: "moderator" }).save();
-      new Role({ name: "admin" }).save();
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "moderator",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'moderator' to roles collection");
+      });
+
+      new Role({
+        name: "admin",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'admin' to roles collection");
+      });
     }
-  } catch (error) {
-    console.log(error);
-  }
+  });
 }
 
 db.mongoose
@@ -65,8 +88,8 @@ app.get("/", (req, res) => {
 });
 
 // routes
-require('./routes/auth.routes')(app);
-require('./routes/user.routes')(app);
+require("./routes/auth.routes")(app);
+require("./routes/user.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
